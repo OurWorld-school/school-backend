044ff190b5dfd089a71d15290ea46b2924aa7ab2
Mac <mac@Macs-MacBook-Pro.local>
2025-03-17 13:57:49 +0100

    update class

--- routes/ClassRoutes.js
+++ routes/ClassRoutes.js
@@ -64,32 +64,7 @@ router.delete("/delete/:id", async (req, res) => {
     res.status(500).json(error);
   }
 });
-router.delete("/delete/:id/admin", async (req, res) => {
-  const { schoolId } = req.params;
-  try {
-    // const schoolId = req.body.schoolId; // Assuming the schoolId is passed in the request body
-    const school = await School.findById(schoolId);
 
-    // Check if the school exists and if the user is an admin
-    if (!school) {
-      return res.status(404).json({ message: "School not found" });
-    }
-
-    if (!school.isAdmin) {
-      return res
-        .status(403)
-        .json({ message: "Only a school director can make this request" });
-    }
-    const classes = await Class.findByIdAndDelete(req.params.id);
-    if (classes) {
-      res.status(200).json({ message: "This Class has been deleted" });
-    } else {
-      res.status(404).json({ message: "Class not found" });
-    }
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
 router.put("/update/:id", async (req, res) => {
   const { id } = req.params;
   const { name } = req.body;
